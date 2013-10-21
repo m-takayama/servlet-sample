d587bd3775e9782cb8b6ebb6854bdfe3bc78575a
Manabu Takayama <learn.libra@gmail.com>
2013-10-21 20:27:27 +0900

    Initial import.

---
+++ hello/src/main/java/jp/vyw/sample/servlet/Hello.java
@@ -0,0 +1,19 @@
+package jp.vyw.sample.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    PrintWriter writer = response.getWriter();
+    writer.println("<html>");
+    writer.println("<body>");
+    writer.println("<p>Hello!</p>");
+    writer.println("</body>");
+    writer.println("</html>");
+  }
+}
